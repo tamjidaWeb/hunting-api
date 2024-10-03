dbce3d76029694478f108bf9250a3626a903edbe
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 13:21:09 +0600

    commited

--- phone.js
+++ phone.js
@@ -1,1 +1,19 @@
-console.log('phone hunting')
+const loadPhone = async()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones);
+
+}
+
+const displayPhones = phones=>{
+        //  console.log(phones);
+        phones.forEach(phone=>{
+            console.log(phone)
+        })
+
+
+}
+
+loadPhone()

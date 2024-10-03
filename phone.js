39e00693e318f2e9bb29d8c40b95959a390ed5ea
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 18:48:18 +0600

    commited

--- phone.js
+++ phone.js
@@ -7,37 +7,68 @@ const loadPhone = async()=>{
 
 }
 
-const displayPhones = phones=>{
-        //  console.log(phones);
 
-        const phonContainer = document.getElementById('phone-container');
-        phones.forEach(phone=>{
-            console.log(phone)
-            // 1 create a div
-            //2 create a div
-            const phoneCard = document.createElement('div');
-            phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
-            //3 set inner html
-            phoneCard.innerHTML = `
+const displayPhones = phones =>{
+    // console.log(phones)
+    const phonContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone)
+        //2. create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 w-96 shadow-xl`;
+        //3 set inner html
+        phoneCard.innerHTML = `
+       
             <figure>
-                      <img
-                        src="${phone.image}"
-                        alt="Shoes" />
-                    </figure>
-                    <div class="card-body">
-                      <h2 class="card-title">${phone.phone_name}</h2>
-                      <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
-                      </div>
-                    </div>
-            `;
-
-            // append child
-            phonContainer.appendChild(phoneCard);
-        })
-
-
+                <img
+                src="${phone.image}"
+                alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+            </div>
+        `;
+        // 4 append child
+        phonContainer.appendChild(phoneCard);
+    })
 }
+
+// const displayPhones = phones=>{
+//         //  console.log(phones);
+
+//         const phonContainer = document.getElementById('phone-container');
+//         phones.forEach(phone=>{
+//             console.log(phone)
+//             // 1 create a div
+//             //2 create a div
+//             const phoneCard = document.createElement('div');
+//             phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
+//             //3 set inner html
+//             phoneCard.innerHTML = `
+//             <figure>
+//                       <img
+//                         src="${phone.image}"
+//                         alt="Shoes" />
+//                     </figure>
+//                     <div class="card-body">
+//                       <h2 class="card-title">${phone.phone_name}</h2>
+//                       <p>If a dog chews shoes whose shoes does he choose?</p>
+//                       <div class="card-actions justify-end">
+//                         <button class="btn btn-primary">Buy Now</button>
+//                       </div>
+//                     </div>
+//             `;
+
+//             // append child
+//             phonContainer.appendChild(phoneCard);
+//         })
+
+
+// }
 
 loadPhone()

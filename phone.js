4df9a4d57bad653fc6f560fdd31387bc75c5a96c
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 13:43:00 +0600

    commited

--- phone.js
+++ phone.js
@@ -9,8 +9,32 @@ const loadPhone = async()=>{
 
 const displayPhones = phones=>{
         //  console.log(phones);
+
+        const phonContainer = document.getElementById('phone-container');
         phones.forEach(phone=>{
             console.log(phone)
+            // 1 create a div
+            //2 create a div
+            const phoneCard = document.createElement('div');
+            phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
+            //3 set inner html
+            phoneCard.innerHTML = `
+            <figure>
+                      <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                      <h2 class="card-title">Shoes!</h2>
+                      <p>If a dog chews shoes whose shoes does he choose?</p>
+                      <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                      </div>
+                    </div>
+            `;
+
+            // append child
+            phonContainer.appendChild(phoneCard);
         })
 
 

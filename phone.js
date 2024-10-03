b2c95d43d6e29516f3d9b6a1732606e24e86b25a
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-03 14:32:26 +0600

    commited

--- phone.js
+++ phone.js
@@ -25,7 +25,7 @@ const displayPhones = phones=>{
                         alt="Shoes" />
                     </figure>
                     <div class="card-body">
-                      <h2 class="card-title">Shoes!</h2>
+                      <h2 class="card-title">${phone.phone_name}</h2>
                       <p>If a dog chews shoes whose shoes does he choose?</p>
                       <div class="card-actions justify-end">
                         <button class="btn btn-primary">Buy Now</button>

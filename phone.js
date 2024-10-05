1f025dee349e1bedf6a79d16c3e31db856e6bb85
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-05 18:51:26 +0600

    commited

--- phone.js
+++ phone.js
@@ -85,7 +85,7 @@ const displayPhones = (phones, isShowAll) =>{
                 <h2 class="card-title">${phone.phone_name}!</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div class="card-actions justify-center">
-                <button onclick= 'handleShowDetail("${phone.slug}")' class="btn btn-primary">Show Details</button>
+                <button onclick= 'handleShowDetail("${phone.slug}");show_details_modal.showModal()' class="btn btn-primary">Show Details</button>
                 </div>
             </div>
             </div>
